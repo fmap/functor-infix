0c8384f97aaa9253eb94be624f2e632c8fb8c8d4
vi <vi@computational.law>
2017-04-23 08:55:12 +0100

    re-export `(<$>)` from prelude rather than defining it
    fixes #3

--- src/Data/Functor/Infix/Definitions.hs
+++ src/Data/Functor/Infix/Definitions.hs
@@ -1,9 +1,13 @@
 {-# LANGUAGE TemplateHaskell #-}
 
-module Data.Functor.Infix.Definitions where
+module Data.Functor.Infix.Definitions
+  ( module Data.Functor.Infix.Definitions
+  , (<$>)
+  ) where
 
 import Control.Monad (msum)
+import Data.Functor ((<$>))
 import Data.Functor.Infix.TH (declareInfixFmapForFunctorCompositionOfDegree, declareFlippedInfixFmapForFunctorCompositionOfDegree)
 
-$(fmap msum $ mapM declareInfixFmapForFunctorCompositionOfDegree [1..20])
+$(fmap msum $ mapM declareInfixFmapForFunctorCompositionOfDegree [2..20])
 $(fmap msum $ mapM declareFlippedInfixFmapForFunctorCompositionOfDegree [1..20])
