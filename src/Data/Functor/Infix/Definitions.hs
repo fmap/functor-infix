60f31b688ee3d512c7349a837282610ef63359bb
John Ericson <John.Ericson@Obsidian.Systems>
2018-11-09 19:04:58 -0500

    Add support for the icecream cone operator

--- src/Data/Functor/Infix/Definitions.hs
+++ src/Data/Functor/Infix/Definitions.hs
@@ -7,7 +7,15 @@ module Data.Functor.Infix.Definitions
 
 import Control.Monad (msum)
 import Data.Functor ((<$>))
-import Data.Functor.Infix.TH (declareInfixFmapForFunctorCompositionOfDegree, declareFlippedInfixFmapForFunctorCompositionOfDegree)
+import Data.Functor.Infix.TH
+ ( declareInfixFmapForFunctorCompositionOfDegree
+ , declareFlippedInfixFmapForFunctorCompositionOfDegree
+ , declareInfixFmapConstForFunctorCompositionOfDegree
+ , declareFlippedInfixFmapConstForFunctorCompositionOfDegree
+ )
 
 $(fmap msum $ mapM declareInfixFmapForFunctorCompositionOfDegree [2..20])
+-- from 1 because `<&>` not found elsewhere
 $(fmap msum $ mapM declareFlippedInfixFmapForFunctorCompositionOfDegree [1..20])
+$(fmap msum $ mapM declareInfixFmapConstForFunctorCompositionOfDegree [2..20])
+$(fmap msum $ mapM declareFlippedInfixFmapConstForFunctorCompositionOfDegree [2..20])
